861a2a781fd781c2c562e923574701592ed9b072
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-08-28 14:58:12 +0100

    feat: setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { AdminModule } from './admin/admin.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, AccountsModule, AdminModule, TransactionsModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
