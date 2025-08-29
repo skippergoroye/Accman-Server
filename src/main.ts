4e3cfe5c4f1c192a3af17607642815e1b4af63ce
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-08-29 16:24:39 +0100

    feat: cors whitelited for local

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://fays-hub-v3.vercel.app'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
