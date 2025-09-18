a811dba52b250e89306f64f55525624e4a72fdbd
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-09-18 11:58:12 +0100

    feat: cors error by passed

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://fays-hub-v3.vercel.app'], 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: '*', // Allow requests from any origin ['http://localhost:5173', 'https://fays-hub-v3.vercel.app'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Include OPTIONS for preflight requests
+    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept', // Explicitly allow common headers
   });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
