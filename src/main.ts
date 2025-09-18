e6ee93c25a7f4545d085b764dd46a51517e0d6a4
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-09-18 13:04:24 +0100

    feat: fixed well for cors error

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*', // Allow requests from any origin ['http://localhost:5173', 'https://fays-hub-v3.vercel.app'], 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Include OPTIONS for preflight requests
-    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
-    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept', // Explicitly allow common headers
+    origin: ['http://localhost:5173', 'https://fays-hub-v3.vercel.app'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
   });
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
